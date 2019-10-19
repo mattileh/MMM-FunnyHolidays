8f485075e2d5d5cb692c69668bcfade24f1b21c3
Matti Lehtinen <matti.lehtinen@symbio.com>
2019-10-19 09:42:24 +0300

    initial commit

---
+++ MMM-FunnyHolidays.js
@@ -0,0 +1,104 @@
+/* global Module */
+
+/* Magic Mirror
+ * Module: FunnyHolidays
+ *
+ * By Matti Lehtinen
+ * MIT Licensed.
+ * TODO: 
+ * translate possibility to config and fallback to a few spesified languages en,es,ger
+ * timezones and API fetch for localizations 
+ * fetch only upon a day changed -> now within every 12hours
+ */
+
+Module.register("MMM-FunnyHolidays", {
+	getTemplateData: function () {
+		return this.config;
+	},
+	// Override start method.
+	start: function () {
+		this.funnyholidaysToday = "No data",
+		this.funnyholidaysTomorrow = "No data"
+	},
+	// Override socket notification handler.
+	socketNotificationReceived: function (notification, payload) {
+		var self = this;
+		if (notification == 'FUNNY_HOLIDAYS_DATA_RESPONSE') {
+			if (this.state == 1) {
+				//fetching today
+				this.funnyholidaysToday = payload;
+				//proceed to fetch tomorrow
+				this.state = 2;
+
+				var tomorrow = new Date();
+				tomorrow.setDate(tomorrow.getDate() + 1);
+				var formattedTomorrow = (self.formatDate(tomorrow));
+
+				self.sendSocketNotification("FUNNY_HOLIDAYS_DATA_REQUEST", { 'date': formattedTomorrow });
+			} else if (this.state == 2) {
+				this.funnyholidaysTomorrow = payload;
+				this.state = 1;
+				//start again after timeout:
+				setTimeout(function () {
+					var today = new Date();
+					var formatted = (self.formatDate(today));
+					self.sendSocketNotification("FUNNY_HOLIDAYS_DATA_REQUEST", { 'date': formatted });
+				}, 60 * 60 * 1000 * 12);
+
+			}
+			this.updateDom();
+		}
+	},
+	// Override socket notification handler.
+	notificationReceived: function (notification, payload, sender) {
+		var self = this;
+		if (notification == 'MODULE_DOM_CREATED') {
+			//fetch today's funny holidays
+			var start = Date.now();
+			var formatted = (self.formatDate(start));
+			self.state = 1;
+			self.sendSocketNotification("FUNNY_HOLIDAYS_DATA_REQUEST", { 'date': formatted });
+		}
+	},
+	formatDate: function (date) {
+		var d = new Date(date),
+			month = '' + (d.getMonth() + 1),
+			day = '' + d.getDate(),
+			year = d.getFullYear();
+
+		if (month.length < 2)
+			month = '0' + month;
+		if (day.length < 2)
+			day = '0' + day;
+
+		return [year, month, day].join('-');
+	},
+	getDom: function () {
+		var wrapper = document.createElement("div");
+
+		var message = document.createElement("div");
+		message.className = "small bright";
+
+		var text = document.createElement("span");
+		//TODO: translate:
+		text.innerHTML = this.translate("TODAY") + ": " + this.funnyholidaysToday;
+
+		message.appendChild(text);
+		wrapper.appendChild(message);
+
+		var subtext = document.createElement("div");
+		subtext.innerHTML = this.translate("TOMORROW") + ": " + this.funnyholidaysTomorrow;
+		subtext.className = "xsmall dimmed";
+		wrapper.appendChild(subtext);
+
+		if(this.config.showSourceLogo){
+			var icon = document.createElement("img");
+			icon.src = "modules/MMM-FunnyHolidays/images/webcalfi-logo-small.png";
+			icon.width = 24;
+			icon.height = 24;
+			wrapper.appendChild(icon);
+		}
+
+		return wrapper;
+	},
+});
---
+++ node_helper.js
@@ -0,0 +1,87 @@
+/* global Module */
+
+/* node_helper.js
+ *
+ * Magic Mirror
+ * Module: MMM-FunnyHolidays
+ * Datasource by http://www.webcal.fi/
+ * Magic Mirror By Michael Teeuw http://michaelteeuw.nl
+ * MIT Licensed.
+ *
+ * Module MMM-FunnyHolidays By Matti Lehtinen
+ * MIT Licensed.
+ */
+
+var NodeHelper = require('node_helper');
+var request = require('request');
+var zlib = require('zlib');
+
+module.exports = NodeHelper.create({
+  getData: function (payload) {
+    var self = this;
+    var options = {
+      method: 'GET',
+      url: 'http://www.webcal.fi/cal.php?id=50&format=json&start_year=current_year&end_year=current_year&tz=Europe%2FHelsinki',
+      headers: {
+        'Host': 'www.webcal.fi',
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate',
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/json; charset=UTF-8',
+        'User-Agent': 'MagicMirrorFunnyHolidaysModule'
+      },
+      encoding: null
+    };
+
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+
+      if (response.headers['content-encoding'] == 'gzip') {
+        zlib.gunzip(body, function (err, dezipped) {
+          var json = JSON.parse(dezipped.toString());
+
+          console.log("PUUKKO date: " + payload.date);
+          var isFirst = false;
+          var text = "nodata";
+
+          for (var i = 0; i < json.length; i++) {
+            if (json[i].date === payload.date) {
+              if (isFirst === false) {
+                text = "";
+                text = text + " " + json[i].name;
+                isFirst = true;
+              }
+              else {
+                text = text + "," + json[i].name;
+              }
+            }
+          }
+
+          self.sendSocketNotification(
+            'FUNNY_HOLIDAYS_DATA_RESPONSE',
+            text);
+        });
+      }
+    });
+  },
+  formatDate: function (date) {
+    var d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+    if (month.length < 2)
+      month = '0' + month;
+    if (day.length < 2)
+      day = '0' + day;
+
+    return [year, month, day].join('-');
+  },
+
+  // subclass socketNotificationReceived, received notification from module
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === 'FUNNY_HOLIDAYS_DATA_REQUEST') {
+      this.getData(payload);
+    }
+  }
+});
